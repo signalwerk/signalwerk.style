0d8ef63d9fcd1ead52284d3938868fbe1f3e2f99
signalwerk <sh@signalwerk.ch>
2023-04-18 01:19:26 +0200

    FIX: readme stamp

--- ci/update-readme.sh
+++ ci/update-readme.sh
@@ -1,64 +1,29 @@
-set -e # Exit with nonzero exit code if anything fails
+# This script obtains the hash of a specific branch, and replaces a 
+# string in the README.md file with the obtained hash. 
+# The string being replaced has the format `REPOSITORY/HASH/`, 
+# where `$REPOSITORY` is the name of the GitHub repository, 
+# and `$HASH` is a 7 character git hash hash of the specified git branch. 
+
+
+# Exit with nonzero exit code if anything fails
+set -e
 
 echo "-- start"
-
-SOURCE_BRANCH="${SOURCE_BRANCH:-main}"
-ROOT_DIR=$(pwd)
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-
+BRANCH="${SOURCE_BRANCH:-main}"
+REPOSITORY="${GITHUB_REPOSITORY#*/}"
 
 echo "   * Variables"
-echo "     – SOURCE_BRANCH=${SOURCE_BRANCH}"
-echo "     – ROOT_DIR=${ROOT_DIR}"
-echo "     – REPO=${REPO}"
-echo "     – SSH_REPO=${SSH_REPO}"
+echo "     – BRANCH=${BRANCH}"
+echo "     – GITHUB_REPOSITORY=${GITHUB_REPOSITORY}"
+echo "     – REPOSITORY=${REPOSITORY}"
 
-# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ $DRONE ]; then
-
-  echo "     – DRONE_BUILD_EVENT=${DRONE_BUILD_EVENT}"
-  echo "     – DRONE_COMMIT_BRANCH=${DRONE_COMMIT_BRANCH}"
-
-  if [ "$DRONE_BUILD_EVENT" != "push" -o "$DRONE_COMMIT_BRANCH" != "$SOURCE_BRANCH" ]; then
-      echo "Skipping deploy; just doing a build."
-      exit 0
-  fi
-fi
-
-
-git config user.name "CI System"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
-
-
-git checkout main
-
-
-
-hash=`git ls-remote git://github.com/signalwerk/signalwerk.styles.git | grep refs/heads/gh-pages | cut -f 1 | awk '{ print substr($1,1,7) }'`
-echo "style-hash: $hash"
+# get the hash
+hash=`git ls-remote | grep refs/heads/$BRANCH | cut -f 1 | awk '{ print substr($1,1,7) }'`
+echo "   * hash: $hash"
 
 # macos
-# sed -i '' -E "s/(signalwerk.styles\/)[a-f0-9]{7}(\/styles)/\1$hash\2/g" README.md
-sed -i -r "s/(signalwerk.styles\/)[a-f0-9]{7}(\/styles)/\1$hash\2/g" README.md
+# sed -i '' -E "s/(${REPOSITORY}\/)[a-f0-9]{7}(\/)/\1$hash\2/g" README.md
+sed -i -r "s/(${REPOSITORY}\/)[a-f0-9]{7}(\/)/\1$hash\2/g" README.md
 
 
-
-git add -A README.md
-git commit -m "Update readme [CI SKIP]"
-
-
-# Get the deploy key by using stored variables to decrypt id_rsa.enc
-# sha256 set to have openssl 1 & 2 compatibility
-eval `ssh-agent -s`
-openssl aes-256-cbc -md sha256 -d -in "$ROOT_DIR/ci/.ssh/id_rsa.enc" -pass "pass:$DECRYPT_KEY" | ssh-add -
-
-
-# add github as known host
-mkdir -p ~/.ssh
-ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
-
-
-# Now that we're all set up, we can push.
-echo "   * push git"
-git push $SSH_REPO $TARGET_BRANCH
+echo "-- end"
